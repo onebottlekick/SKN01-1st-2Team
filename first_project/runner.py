0a0794427e6d0536369d54d14e2755f7ff81e4a1
rainfrog2301 <hgm718@naver.com>
2024-05-27 16:40:49 +0900

    중고차크크롤링

--- first_project/runner.py
+++ first_project/runner.py
@@ -1,1 +1,112 @@
 # runner.py
+import os
+import time
+import pandas as pd
+from util.util import User
+
+# 사이트 접속 -> 검색버튼 클릭 -> 전기친환경 클릭 -> 개인 클릭 ->
+if __name__ == "__main__":
+    user = User()
+    user.getBrowser("http://www.encar.com/", new_window=True)
+    time.sleep(2)
+    # user.search_keword('//*[@id="AKCSearch"]', '맥북')
+    user.click_button('//*[@id="evmenu"]')  # 전기친환경
+    time.sleep(2)
+    user.click_button(
+        '//*[@id="rySch_car"]/div[2]/fieldset/div[11]/h5/a'
+    )  # 판매자 구분
+    time.sleep(2)
+    user.scroll_down(500)  # 내려서
+    time.sleep(2)
+    user.click_button(
+        '//*[@id="schBuytype"]/div/ul/li[1]/label'
+    )  # 개인 판매자 체크박스
+    time.sleep(2)
+    # user.click_button('//*[@id="searchCondition2"]/div[6]/div[2]/a') # 검색버튼
+    # # user.scroll_down(200)
+    # # user.click_button('//*[@id="opinionDESC"]')  # 상품평 많은 순 --> 생략
+    # time.sleep(3)
+    # user.click_button('/html/body/div[2]/div[4]/div[3]/div[2]/div[8]/div[1]/div[2]/div[3]/ul/li[1]/div/div[2]/p/a')  # 상품이름 클릭
+
+    # time.sleep(3)
+    # user.change_window() # 탭 전환
+    # user.scroll_down(600, init_pos=True)
+    # user.click_button('//*[@id="bookmark_cm_opinion_item"]/a/h3') # 의견/리뷰 누르기
+    # user.click_button('//*[@id="danawa-prodBlog-productOpinion-button-tab-companyReview"]') # 쇼핑몰 상품 리뷰 클릭
+    # time.sleep(3)
+    # user.scroll_down(300)
+
+    name_lst = []
+    year_lst = []
+    kilo_lst = []
+    price_lst = []
+    
+
+    for i in range(17):
+        try:
+            if i <= 9:
+                user.click_button(f"/html/body/div[1]/div[2]/div[1]/div[2]/div[3]/div[1]/div[4]/div[5]/span[{i+1}]/a") #다음페이지
+            elif 9 < i <= 17:
+                user.click_button(f"/html/body/div[1]/div[2]/div[1]/div[2]/div[3]/div[1]/div[4]/div[5]/span[{i-8}]/a") #다음페이지
+            time.sleep(3)
+            try:
+                for j in range(21):  # 제목이나 사이트 등 더 추가하고 싶다면 여기서 추가 입력
+                    name = f"/html/body/div[1]/div[2]/div[1]/div[2]/div[3]/div[1]/div[4]/div[3]/table/tbody/tr[{j+1}]/td[2]/a"  # 차이름
+                    year = f"/html/body/div[1]/div[2]/div[1]/div[2]/div[3]/div[1]/div[4]/div[3]/table/tbody/tr[{j+1}]/td[2]/span[1]/span[1]"  # 연식
+                    kilo = f"/html/body/div[1]/div[2]/div[1]/div[2]/div[3]/div[1]/div[4]/div[3]/table/tbody/tr[{j+1}]/td[2]/span[1]/span[2]"  # 킬로수
+                    price = f"/html/body/div[1]/div[2]/div[1]/div[2]/div[3]/div[1]/div[4]/div[3]/table/tbody/tr[{j+1}]/td[3]/strong"  # 가격
+                    
+                    name = user.find_ele_text(name)
+                    print(name)
+                    year = user.find_ele_text(year)
+                    print(year)
+                    kilo = user.find_ele_text(kilo)
+                    print(kilo)
+                    price = user.find_ele_text(price)
+                    print(price)
+                    
+                    name_lst.append(name)
+                    year_lst.append(year)
+                    kilo_lst.append(kilo)
+                    price_lst.append(price)
+                    
+            except:
+                pass
+
+            if i==9:
+                user.click_button("/html/body/div[1]/div[2]/div[1]/div[2]/div[3]/div[1]/div[4]/div[5]/span[11]")
+                print(f'------------ {i+1}페이지 finished!! -------------')
+                print(f'------------ 11페이지 부터 시작!!-------------')
+                
+            else:
+                print(f'------------ {i+1}페이지 finished!! -------------')
+                # time.sleep(2)
+                # user.click_button(f'/html/body/div[2]/div[5]/div[2]/div[4]/div[4]/div/div[3]/div[2]/div[3]/div[2]/div[5]/div/div/div/a[{i+1}]')
+
+                ## 오류가 났던 이유
+                # 나머지 페이지들의  PATH : /html/body/div[2]/div[5]/div[2]/div[4]/div[4]/div/div[3]/div[2]/div[3]/div[2]/div[5]/div/div/div/a[1] # page 1
+                # 마지막 페이지(6쪽) PATH : /html/body/div[2]/div[5]/div[2]/div[4]/div[4]/div/div[3]/div[2]/div[3]/div[2]/div[5]/div/div/div/span
+        except:
+            pass
+    name_lst = [name for name in name_lst if name.strip()]
+    year_lst = [year for year in year_lst if year.strip()]
+    kilo_lst = [kilo for kilo in kilo_lst if kilo.strip()]
+    price_lst = [price for price in price_lst if price.strip()]
+    user.close_connect()
+    print(f"차량 {len(name_lst)}대가 저장되었습니다.")
+
+    df = pd.DataFrame(
+        {
+            "차 이름": name_lst,
+            "연식": year_lst,
+            "키로수": kilo_lst,
+            "가격": price_lst,
+        }
+    )
+
+    # result 파일 경로 만들기
+    result_path = os.path.join(os.getcwd(), "result")
+    os.makedirs(result_path, exist_ok=True)
+    # csv로 저장
+    df.to_csv(os.path.join(result_path, "review.csv"), index=False, encoding="utf-8")
+    print(f'#### "review.csv"가 {result_path}에 저장되었습니다. ####')
--- first_project/util/util.py
+++ first_project/util/util.py
@@ -1,1 +1,93 @@
 # util.py
+import selenium
+import subprocess
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+
+### 드라이버 변경하기
+chrome_driver_path = r'/usr/local/bin/chromedriver'
+
+class User:
+    def __init__(self):
+        self.new_window = True
+        self.options = Options()
+        # detach True 옵션 안끄면 크롬창 계속 열려있으니 원치 않는다면 주석처리하세요(코드 여러번 돌리면 크롬창이 매우매우 많이 생성됩니다..)
+        self.options.add_argument("--start-maximized")
+        self.options.add_experimental_option("detach", True)
+        self.options.add_argument("--disable-blink-features=AutomationControlled")
+        self.browser  = webdriver.Chrome(options=self.options)
+        self.browser.maximize_window()
+        self.scroll_pos = 0 
+        # service = Service(chrome_driver_path) 버전 업데이트 되면서 필요없어짐
+
+    def getBrowser(self, url='https://naver.com', new_window=True, option=None):  
+        if new_window == False: # 기존 브라우저 모드
+            print('디버깅 모드를 시작합니다.')
+            time.sleep(2)
+            option = self.options
+            option.add_experimental_option('debuggerAddress','127.0.0.1:9222')
+            
+            # 디버거모드로 실행해보고 싶으면, 본인 랩탑 chrome.exe 경로 찾아서 변경하기 --> 파라미터 new_window=False
+            # cmd = ['chrome.exe', '--remote-debugging-port=9222', '--user-data-dir="C:\\Program Files\\Google\\Chrome\\Temp"']
+            # chrome_directory = 'C:\\Program Files\\Google\\Chrome\\Application'
+            # subprocess.run(cmd, cwd=chrome_directory, shell=True)
+
+        else:
+            self.browser.get(url)
+            
+    # 해당 XPATH의 요소의 text 반환하는 함수
+    def find_ele_text(self,user_xpath):
+        text = self.browser.find_element(By.XPATH, user_xpath).text.strip()
+        return text
+
+    
+
+    # item_name을 파라미터로 입력받아, 검색창에 입력하는 함수
+    def search_keword(self,user_xpath, item_name):
+        self.browser.find_element(By.XPATH, user_xpath).send_keys(item_name)
+        
+    # 버튼 클릭 함수 (모든 클릭해야 하는 부분에서 사용함)
+    # 웹페이지 버튼의 role을 보면 link인 것이 있고 아닌 것이 있었음. 
+    # -> 해당 부분에서 오류가나는가 싶어서 구글링해서 By.PARTIAL_LINK_TEXT 가져옴
+    def click_button(self,button_xpath=None):
+        try:
+            self.browser.find_element(By.XPATH, button_xpath).click()
+            print('버튼 클릭 성공')
+        except Exception as e:
+            print('By.XPATH로 클릭되지 않습니다. By.PARTIAL_LINK_TEXT로 접근합니다! ')
+            time.sleep(2)
+            try:
+                totext = self.find_ele_text(button_xpath) # 버튼 이름 텍스트로 가져와서
+                self.browser.find_element(By.PARTIAL_LINK_TEXT, totext).click() # By.PARTIAL_LINK_TEXT 안에 넣어줌
+    
+                print('성공!')
+                if totext == None: # 텍스트 받아오기를 실패하면 직접 이름 입력
+                    print('해당 버튼의 키워드(이름)를 추가로 입력하세요. --> click_button(XPATH, 버튼 이름)')
+                    
+            except Exception as e:
+                print('문제발생!', type(e))
+
+    # 스크롤 내려주는 함수 
+    # 픽셀값이 0이면 최상단. pixel 파라미터값만큼 이동 (defualt는 500씩 이동하도록 설정)
+    def scroll_down(self, pixel=500, init_pos=False): 
+        try:
+            if init_pos == True: # 새로운 탭으로 전환할 경우 스크롤 위치 픽셀값을 최상단으로 초기화하기 위해 지정
+                self.scroll_pos = 0         
+
+            self.browser.execute_script(f"window.scrollTo({self.scroll_pos}, {self.scroll_pos+pixel})") # (2) 해당위치에서 pixel만큼 이동하도록
+            self.scroll_pos = self.scroll_pos + pixel # (1) 스크롤값이 계속 저장되어서 ----------------------↑
+            print('스크롤 다운 성공')
+
+        except Exception as e:
+            print('스크롤 다운 실패') 
+            print(e)   
+
+    def close_connect(self): # 창이 닫히는 게 아닌 연결이 끊기는 것
+        self.browser.close()
+        print('finished.')
+        print(' ')
+
+    def change_window(self):
+        self.browser.switch_to.window(self.browser.window_handles[-1]) # 가장 마지막에 열린 창으로 이동
